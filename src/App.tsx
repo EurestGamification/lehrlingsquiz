0b7ab671cfeab82715d74f1614b9c1b48fe590af
Behemoth <simonostini@gmail.com>
2022-02-12 15:00:10 +0100

    added first test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "./assets/img/logo.png";
 import "./App.scss";
+import Test from "./components/test/test";
 
 const App = () => {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
+      <Test />
     </div>
   );
 };
---
+++ src/components/test/test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface TestProps {}
+
+const Test: React.FC<TestProps> = ({}: TestProps) => {
+  return <div>test</div>;
+};
+
+export default Test;
