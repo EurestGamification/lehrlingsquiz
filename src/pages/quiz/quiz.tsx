710925c6de595b671b5cac4fb61bd54d611d949a
Behemoth <simonostini@gmail.com>
2022-02-13 17:25:04 +0100

    added stepper

--- src/pages/quiz/quiz.tsx
+++ src/pages/quiz/quiz.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@lehrlingsquiz/components";
+import { Step, StepLabel, Stepper } from "@mui/material";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import { quizStore } from "src/stores/quize.store";
@@ -8,15 +9,51 @@ interface QuizProps {}
 
 const Quiz: React.FC = inject(quizStore.storeKey)(
   observer(({}: QuizProps) => {
+    const steps: any[] = [
+      "Erkennen von Lebensmitteln",
+      "Zubereitungsarten & Menüerkennung",
+      "Kundenorientierung",
+      "Quiz zum Unternehmen Eurest (Schätzfragen)"
+    ];
+
     return (
       <div className="quiz">
         <Header />
         <div className="quiz__content">
-          <div>{quizStore.score}</div>
+          <Stepper
+            alternativeLabel
+            activeStep={quizStore.currentQuizStep}
+          >
+            {steps.map((label: string) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+
+          <button
+            onClick={() =>
+              quizStore.setCurrentQuizStep(
+                quizStore.currentQuizStep - 1
+              )
+            }
+          >
+            Zurück
+          </button>
+          <button
+            onClick={() =>
+              quizStore.setCurrentQuizStep(
+                quizStore.currentQuizStep + 1
+              )
+            }
+          >
+            Weiter
+          </button>
+
           <button
             onClick={() => quizStore.setScore(quizStore.score + 1)}
           >
-            increase
+            increase score
           </button>
         </div>
       </div>
--- src/stores/quize.store.ts
+++ src/stores/quize.store.ts
@@ -4,7 +4,15 @@ import { IStore, StoreKey } from "./interfaces/store";
 export class QuizStore implements IStore {
   storeKey: StoreKey = "quizStore" as const;
 
+  quizSteps = [
+    "Erkennen von Lebensmitteln",
+    "Zubereitungsarten & Menüerkennung",
+    "Kundenorientierung",
+    "Quiz zum Unternehmen Eurest (Schätzfragen)"
+  ] as const;
+
   @observable score: number = 0;
+  @observable currentQuizStep: number = 0;
 
   constructor() {
     makeAutoObservable(this);
@@ -12,6 +20,14 @@ export class QuizStore implements IStore {
 
   @action setScore(value: number): void {
     this.score = value;
+  }
+
+  @action setCurrentQuizStep(value: number): number {
+    if (value < 0) return (this.currentQuizStep = 0);
+    if (value > this.quizSteps.length)
+      return (this.currentQuizStep = this.quizSteps.length);
+
+    return (this.currentQuizStep = value);
   }
 }
 
