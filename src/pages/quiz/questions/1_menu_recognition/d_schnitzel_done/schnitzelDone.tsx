746ba3e492b8b843053cf1fbe24f30a3be517e59
Behemoth <simonostini@gmail.com>
2022-02-14 22:59:09 +0100

    added 2. category ending scene

--- src/pages/quiz/questions/1_menu_recognition/d_schnitzel_done/schnitzelDone.tsx
+++ src/pages/quiz/questions/1_menu_recognition/d_schnitzel_done/schnitzelDone.tsx
@@ -2,6 +2,8 @@ import { quizStore } from "@lehrlingsquiz/stores";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import { IMenuRecognitionProps } from "../interfaces";
+import wienerSchnitzel from "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg";
+import "./schnitzelDone.scss";
 
 interface SchnitzelDoneProps extends IMenuRecognitionProps {}
 
@@ -12,7 +14,11 @@ const SchnitzelDone: React.FC<SchnitzelDoneProps> = inject(
     return (
       <div className="schnitzel-done">
         <div className="schnitzel-done__content">
-          SchnitzelDone
+          <img src={wienerSchnitzel} alt="Wiener Schnitzel" />
+          <p className="schnitzel-done__content__info">
+            Geschafft! Das hast du dir jetzt redlich verdient!
+          </p>
+
           <button
             onClick={() =>
               quizStore.setCurrentQuizStep(
