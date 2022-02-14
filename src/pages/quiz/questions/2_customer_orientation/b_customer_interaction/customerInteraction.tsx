14dea11766497c406be41c06f49e6ee59d98306a
Behemoth <simonostini@gmail.com>
2022-02-15 00:25:53 +0100

    added 3b

--- src/pages/quiz/questions/2_customer_orientation/b_customer_interaction/customerInteraction.tsx
+++ src/pages/quiz/questions/2_customer_orientation/b_customer_interaction/customerInteraction.tsx
@@ -2,27 +2,32 @@ import React from "react";
 import { ICustomerOrientationProps } from "../customerOrientation";
 import "./customerInteraction.scss";
 import customerOrdering from "@lehrlingsquiz/assets/img/customer_ordering.jpg";
+import { quizStore } from "@lehrlingsquiz/stores";
+import { inject, observer } from "mobx-react";
 
 interface CustomerInteractionProps
   extends ICustomerOrientationProps {}
 
-const CustomerInteraction: React.FC<CustomerInteractionProps> = ({
-  onStepFinished
-}: CustomerInteractionProps) => {
-  return (
-    <div className="customer-interaction">
-      <div className="customer-interaction__content">
-        <img src={customerOrdering} alt="Gast bestellt" />
-        <button
-          onClick={() => {
-            onStepFinished();
-          }}
-        >
-          Weiter
-        </button>
-      </div>
-    </div>
+const CustomerInteraction: React.FC<CustomerInteractionProps> =
+  inject(quizStore.storeKey)(
+    observer(() => {
+      return (
+        <div className="customer-interaction">
+          <div className="customer-interaction__content">
+            <img src={customerOrdering} alt="Gast bestellt" />
+            <button
+              onClick={() => {
+                quizStore.setCurrentQuizStep(
+                  quizStore.currentQuizStep + 1
+                );
+              }}
+            >
+              Weiter
+            </button>
+          </div>
+        </div>
+      );
+    })
   );
-};
 
 export default CustomerInteraction;
