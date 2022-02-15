4656b5afb3b3c9fddf7111754acb2feef0b7fce5
Tabipool <tabea.gsch@gmail.com>
2022-02-15 01:40:43 +0100

    added step to next question (4)

--- src/pages/quiz/questions/3_company_estimate/companyEstimate.tsx
+++ src/pages/quiz/questions/3_company_estimate/companyEstimate.tsx
@@ -1,6 +1,6 @@
 import { quizStore } from "@lehrlingsquiz/stores";
 import { inject, observer } from "mobx-react";
-import React from "react";
+import React, { useState } from "react";
 import "./companyEstimate.scss";
 
 /*const startQuiz = async () =>{
@@ -34,24 +34,35 @@ const questions = [
   }
 ]
 
-const checkAnswer = (isCorrect: boolean) => {
-  if (isCorrect){
-    quizStore.setScore(quizStore.score + 1);
-  }
-}
-
 export const CompanyEstimate: React.FC = inject(quizStore.storeKey)(
   observer(() => {
-   
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    const checkAnswer = (isCorrect: boolean) => {
+      if (isCorrect){
+        quizStore.setScore(quizStore.score + 1);
+      }
+
+      const nextQuestion = currentQuestion + 1;
+      if (nextQuestion < questions.length) {
+        setCurrentQuestion(nextQuestion);
+      }
+      else{
+        quizStore.setCurrentQuizStep(
+          quizStore.currentQuizStep + 1 /*Show Result-Page*/
+        )
+      }
+    }
+
     return (
       <div className="company-estimate">
         <h3>{quizStore.quizSteps[3]}</h3>
         <div className="company-estimate__content">
           <p className="company-estimate__content__instruction">
-            {questions[0].questionText}
+            {questions[currentQuestion].questionText}
           </p>
           <div className="answer-section">
-            {questions[0].answerOptions.map((answerOption) => <button onClick={() => checkAnswer(answerOption.isCorrect)} className="answer-item">{answerOption.answerText}</button>)}
+            {questions[currentQuestion].answerOptions.map((answerOption) => <button onClick={() => checkAnswer(answerOption.isCorrect)} className="answer-item">{answerOption.answerText}</button>)}
           </div>
           <button
             onClick={() => {
