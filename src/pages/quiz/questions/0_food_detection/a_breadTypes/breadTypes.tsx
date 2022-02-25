ea5916c0957be16d3a4950dffd1c5e74e4399475
Timon Stadelmann <stadelmann.timon@gmail.com>
2022-02-25 01:45:26 +0100

    vegetableTypes and breadTypes

--- src/pages/quiz/questions/0_food_detection/a_breadTypes/breadTypes.tsx
+++ src/pages/quiz/questions/0_food_detection/a_breadTypes/breadTypes.tsx
@@ -14,12 +14,47 @@ import {
 } from "@dnd-kit/core";
 import { Droppable } from "../dnd/droppable";
 import { Draggable } from "../dnd/draggable";
+import lodash from "lodash";
+
+// ! DnD out of position when scrolling
+// TODO make droppables to sortables
+// TODO make drag overlay with modifier for 'restrictWindowEdge'
+// TODO color draggables border green if correct and red if wrong
 
 interface BreadTypesProps extends IFoodDetectionProps {}
 
 interface DnDs {
-  [key: string]: Array<string>;
+  [key: string]: Array<Bread>;
 }
+
+// eslint-disable-next-line no-unused-vars
+enum Bread {
+  // eslint-disable-next-line no-unused-vars
+  Toastbrot,
+  // eslint-disable-next-line no-unused-vars
+  Baguette,
+  // eslint-disable-next-line no-unused-vars
+  Laugenstangerl,
+  // eslint-disable-next-line no-unused-vars
+  Semmel,
+  // eslint-disable-next-line no-unused-vars
+  Roggenbrot,
+  // eslint-disable-next-line no-unused-vars
+  Vollkornbrot
+}
+
+// eslint-disable-next-line no-unused-vars
+const breadSources: { [key in Bread]: string } = {
+  [Bread.Toastbrot]: "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg",
+  [Bread.Baguette]: "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg",
+  [Bread.Laugenstangerl]:
+    "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg",
+  [Bread.Semmel]: "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg",
+  [Bread.Roggenbrot]:
+    "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg",
+  [Bread.Vollkornbrot]:
+    "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg"
+};
 
 const BreadTypes: React.FC<BreadTypesProps> = ({
   onStepFinished
@@ -31,19 +66,20 @@ const BreadTypes: React.FC<BreadTypesProps> = ({
 
   const DnDsSolution: DnDs = {
     Start: [],
-    Weißbrot: ["Semmel", "Toastbrot"],
-    Gebäck: ["Laugenstangerl"],
-    Schwarzbrot: ["Roggenbrot", "Vollkornbrot"]
+    Weißbrot: [Bread.Baguette, Bread.Toastbrot],
+    Gebäck: [Bread.Laugenstangerl, Bread.Semmel],
+    Schwarzbrot: [Bread.Roggenbrot, Bread.Vollkornbrot]
   };
 
   const [currentDnDs, setCurrentDnDs] = useState<DnDs>({
-    Start: [
-      "Laugenstangerl",
-      "Toastbrot",
-      "Semmel",
-      "Vollkornbrot",
-      "Roggenbrot"
-    ],
+    Start: lodash.shuffle<Bread>([
+      Bread.Laugenstangerl,
+      Bread.Baguette,
+      Bread.Toastbrot,
+      Bread.Semmel,
+      Bread.Vollkornbrot,
+      Bread.Roggenbrot
+    ]),
     Weißbrot: [],
     Gebäck: [],
     Schwarzbrot: []
@@ -57,9 +93,7 @@ const BreadTypes: React.FC<BreadTypesProps> = ({
         Brotkorb fallen lassen.
       </p>
       <div className="breadTypes__content">
-        {/* // ! DnD out of position when scrolling */}
         <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
-          {/* // TODO make droppables to sortables */}
           {Object.keys(currentDnDs).map((breadType, bTi) => (
             <Droppable
               id={breadType}
@@ -74,14 +108,16 @@ const BreadTypes: React.FC<BreadTypesProps> = ({
                   {breadType}
                 </span>
               )}
-              {/* // TODO make drag overlay with modifier for 'restrictWindowEdge' */}
-              {currentDnDs[breadType].map((bread, bi) => (
+              {currentDnDs[breadType].map((bread) => (
                 <Draggable
-                  id={bread}
-                  key={bi}
+                  id={Bread[bread]}
+                  key={bread}
                   className="breadTypes__content__draggable"
                 >
-                  {bread}
+                  <img
+                    src={breadSources[bread]}
+                    alt="Brot - Draggable"
+                  />
                 </Draggable>
               ))}
             </Droppable>
@@ -95,13 +131,16 @@ const BreadTypes: React.FC<BreadTypesProps> = ({
   function handleDragEnd(event: DragEndEvent) {
     if (event.over) {
       for (const breadType in currentDnDs) {
-        const index = currentDnDs[breadType].indexOf(event.active.id);
+        const eventBread: Bread =
+          Bread[event.active.id as keyof typeof Bread];
+        const index = currentDnDs[breadType].indexOf(eventBread);
+
         if (index > -1) {
           if (breadType === event.over.id) return;
 
           const tempDnDs: DnDs = { ...currentDnDs };
           tempDnDs[breadType].splice(index, 1);
-          tempDnDs[event.over.id].push(event.active.id);
+          tempDnDs[event.over.id].push(eventBread);
           setCurrentDnDs(tempDnDs);
           return;
         }
@@ -113,20 +152,15 @@ const BreadTypes: React.FC<BreadTypesProps> = ({
     let correct: number = 0;
     for (const breadType in DnDsSolution) {
       for (const bread of DnDsSolution[breadType]) {
-        if (currentDnDs[breadType].indexOf(bread) > -1) correct++;
+        currentDnDs[breadType].indexOf(bread) > -1 && correct++;
       }
     }
 
-    // TODO color draggables border green if correct and red if wrong
-
-    const DnDsCount: number = Object.keys(DnDsSolution).reduce(
-      (sum, k) => sum + DnDsSolution[k].length,
-      0
-    );
-    const points: number = correct === DnDsCount ? 1 : 0;
+    const BreadCount: number = Object.keys(Bread).length / 2;
+    const points: number = correct === BreadCount ? 1 : 0;
 
     points && quizStore.setScore(quizStore.score + 1);
-    // alert(`Points: ${points}\nCorrect: ${correct}/${DnDsCount}`);
+    // * dev alert(`Points: ${points}\nCorrect: ${correct}/${BreadCount}`);
     onStepFinished();
   }
 };
--- src/pages/quiz/questions/0_food_detection/b_vegetableTypes/vegetableTypes.tsx
+++ src/pages/quiz/questions/0_food_detection/b_vegetableTypes/vegetableTypes.tsx
@@ -1,34 +1,199 @@
-import React from "react";
+import React, { useState } from "react";
 import { IFoodDetectionProps } from "../foodDetection";
 import "./vegetableTypes.scss";
 import { quizStore } from "@lehrlingsquiz/stores";
-import { inject, observer } from "mobx-react";
+import {
+  DndContext,
+  DragEndEvent,
+  MouseSensor,
+  SensorDescriptor,
+  SensorOptions,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from "@dnd-kit/core";
+import { Droppable } from "../dnd/droppable";
+import { Draggable } from "../dnd/draggable";
+
+// ! DnD out of position when scrolling
+// TODO make droppables to sortables
+// TODO make drag overlay with modifier for 'restrictWindowEdge'
+// TODO color draggables border green if correct and red if wrong
 
 interface VegetableTypesProps extends IFoodDetectionProps {}
 
-const VegetableTypes: React.FC<VegetableTypesProps> = inject(
-  quizStore.storeKey
-)(
-  observer(() => {
-    return (
-      <div className="vegetableTypes">
-        <p className="vegetableTypes__heading">
-          Als Koch:Köchin arbeitet man täglich mit frischem Gemüse.
-          Erkennst du die verschiedenen Gemüsesorten? Ziehe die
-          Bezeichnung zu dem richtigen Gemüse.
-        </p>
-        <button
-          onClick={() =>
-            quizStore.setCurrentQuizStep(
-              quizStore.currentQuizStep + 1
-            )
-          }
-        >
-          Weiter
-        </button>
+interface DnDs {
+  [key: string]: Vegetable | null;
+}
+
+// eslint-disable-next-line no-unused-vars
+enum Vegetable {
+  // eslint-disable-next-line no-unused-vars
+  Karotte,
+  // eslint-disable-next-line no-unused-vars
+  Zucchini,
+  // eslint-disable-next-line no-unused-vars
+  Kohlrabi,
+  // eslint-disable-next-line no-unused-vars
+  Radieschen,
+  // eslint-disable-next-line no-unused-vars
+  Lauch,
+  // eslint-disable-next-line no-unused-vars
+  Jungzwiebel
+}
+
+// eslint-disable-next-line no-unused-vars
+const vegetableSources: { [key in Vegetable]: string } = {
+  [Vegetable.Karotte]:
+    "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg",
+  [Vegetable.Zucchini]:
+    "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg",
+  [Vegetable.Kohlrabi]:
+    "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg",
+  [Vegetable.Radieschen]:
+    "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg",
+  [Vegetable.Lauch]: "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg",
+  [Vegetable.Jungzwiebel]:
+    "@lehrlingsquiz/assets/img/wiener_schnitzel.jpg"
+};
+
+const VegetableTypes: React.FC<VegetableTypesProps> = () => {
+  const sensors: SensorDescriptor<SensorOptions>[] = useSensors(
+    useSensor(TouchSensor),
+    useSensor(MouseSensor)
+  );
+
+  const [currentStart, setCurrentStart] = useState<Array<Vegetable>>([
+    Vegetable.Karotte,
+    Vegetable.Zucchini,
+    Vegetable.Kohlrabi,
+    Vegetable.Radieschen,
+    Vegetable.Lauch,
+    Vegetable.Jungzwiebel
+  ]);
+
+  const [currentDnDs, setCurrentDnDs] = useState<DnDs>({
+    [Vegetable.Karotte]: null,
+    [Vegetable.Zucchini]: null,
+    [Vegetable.Kohlrabi]: null,
+    [Vegetable.Radieschen]: null,
+    [Vegetable.Lauch]: null,
+    [Vegetable.Jungzwiebel]: null
+  });
+
+  return (
+    <div className="vegetables">
+      <p className="vegetables__heading">
+        Als Koch:Köchin arbeitet man täglich mit frischem Gemüse.
+        Erkennst du die verschiedenen Gemüsesorten? Ziehe die
+        Bezeichnung zu dem richtigen Gemüse.
+      </p>
+      <div className="vegetables__content">
+        <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
+          <Droppable
+            id="Start"
+            className="vegetables__content__droppable vegetables__content__droppable__start"
+          >
+            {currentStart.map((vegetable) => (
+              <Draggable
+                id={Vegetable[vegetable]}
+                key={vegetable}
+                className="vegetables__content__draggable"
+              >
+                {Vegetable[vegetable]}
+              </Draggable>
+            ))}
+          </Droppable>
+
+          {Object.keys(currentDnDs).map((vegetableType, vTi) => (
+            <Droppable
+              id={Vegetable[vegetableType as keyof typeof Vegetable]}
+              key={vTi}
+              className="vegetables__content__droppable vegetables__content__droppable__vegetable"
+            >
+              <img
+                src={
+                  vegetableSources[
+                    vegetableType as unknown as keyof typeof vegetableSources
+                  ]
+                }
+                className="vegetables__content__droppable__vegetable__img"
+                alt="Vegetable - Droppable"
+              />
+              {currentDnDs[vegetableType] != null && (
+                <Draggable
+                  id={
+                    Vegetable[vegetableType as keyof typeof Vegetable]
+                  }
+                  className="vegetables__content__draggable"
+                >
+                  {Vegetable[currentDnDs[vegetableType] ?? 0]}
+                </Draggable>
+              )}
+            </Droppable>
+          ))}
+        </DndContext>
       </div>
-    );
-  })
-);
+
+      <button onClick={() => checkAnswers()}>Weiter</button>
+    </div>
+  );
+
+  function handleDragEnd(event: DragEndEvent) {
+    if (event.over) {
+      const index = currentStart.indexOf(
+        Vegetable[event.active.id as keyof typeof Vegetable]
+      );
+      if (index > -1) {
+        if (event.active.id === event.over.id) return;
+        currentStart.splice(index, 1);
+        setCurrentStart(currentStart);
+      } else {
+        for (const vegetable in currentDnDs) {
+          if (
+            currentDnDs[vegetable] ===
+            Vegetable[event.active.id as keyof typeof Vegetable]
+          ) {
+            if (
+              Vegetable[vegetable as keyof typeof Vegetable] ===
+              Vegetable[event.over.id as keyof typeof Vegetable]
+            ) {
+              return;
+            }
+            currentDnDs[vegetable] = null;
+            setCurrentDnDs(currentDnDs);
+          }
+        }
+      }
+
+      if (event.over.id === "Start") {
+        currentStart.push(
+          Vegetable[event.over.id as keyof typeof Vegetable]
+        );
+        setCurrentStart(currentStart);
+      } else {
+        currentDnDs[
+          Vegetable[event.over.id as keyof typeof Vegetable]
+        ] = Vegetable[event.active.id as keyof typeof Vegetable];
+      }
+      setCurrentDnDs(currentDnDs);
+    }
+  }
+
+  function checkAnswers() {
+    let correct: number = 0;
+    for (const vegetable in currentDnDs) {
+      Vegetable[vegetable as keyof typeof Vegetable] ===
+        currentDnDs[vegetable] && correct++;
+    }
+
+    const VegetableCount: number = Object.keys(Vegetable).length / 2;
+    const points: number = correct === VegetableCount ? 1 : 0;
+
+    points && quizStore.setScore(quizStore.score + 1);
+    // * dev alert(`Points: ${points}\nCorrect: ${correct}/${VegetableCount}`);
+    quizStore.setCurrentQuizStep(quizStore.currentQuizStep + 1);
+  }
+};
 
 export default VegetableTypes;
