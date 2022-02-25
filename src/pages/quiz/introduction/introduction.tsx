c4e338a6d86ac31fedb408ee619cae04c49d06e5
Behemoth <simonostini@gmail.com>
2022-02-25 13:49:44 +0100

    update

--- src/pages/quiz/introduction/introduction.tsx
+++ src/pages/quiz/introduction/introduction.tsx
@@ -9,16 +9,18 @@ const Introduction: React.FC = inject(quizStore.storeKey)(
   observer(() => {
     return (
       <div className="introduction">
-        <img
-          className="introduction__claim"
-          src={claim}
-          alt="Eurest Claim"
-        />
-        <img
-          className="introduction__apprentice"
-          src={apprentice}
-          alt="Eurest apprentice"
-        />
+        <div className="introduction__images">
+          <img
+            className="introduction__images__claim"
+            src={claim}
+            alt="Eurest Claim"
+          />
+          <img
+            className="introduction__images__apprentice"
+            src={apprentice}
+            alt="Eurest apprentice"
+          />
+        </div>
         <p className="introduction__instruction">
           some intro text...
         </p>
